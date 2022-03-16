656fd9402f5f08137f5b2bc5ef58fd5f0f2c9426
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-03-16 10:35:59 +0900

    [Update]chapter9(テンプレート)

--- pybotweb.py
+++ pybotweb.py
@@ -1,7 +1,9 @@
-from bottle import route, run
+from bottle import route, run, template # 関数をインポート
+from datetime import datetime # datetimeをインポート
 
 @route('/hello')
 def hello():
-    return 'Hello World!'
+    now = datetime.now() # 現在時刻を取得
+    return template('Hello World! {{now}}', now=now) # template関数の利用
 
 run(host='localhost', port=8080, debug=True)
