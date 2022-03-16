abd6a8e324f824b921934a66a08831490f92f66c
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-03-16 13:01:29 +0900

    [Fix]エラー修正

--- pybotweb.py
+++ pybotweb.py
@@ -2,12 +2,12 @@ from bottle import route, run, template, request # 関数をインポート
 from pybot import pybot
 @route('/hello')
 def hello():
-    return template('pybot_template', input_text='', output_text="") # template関数の利用
+    return template('pybot_template', input_text='', output_text='') # template関数の利用
 
 @route('/hello', method='POST')
 def do_hello():
     input_text = request.forms.input_text
-    output_text = pybot(input)
+    output_text = pybot(input_text)
     return template('pybot_template', input_text=input_text, output_text=output_text)
 
 run(host='localhost', port=8080, debug=True)
